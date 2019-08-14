7bae607316750b3928719fcfe37e8e7706e9aa92
Chelsea <csmith@realestateone.com>
2019-08-14 15:05:15 -0400

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Design and Code React Apps</h1>
+    <p>Welcome to your new site!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the videos</Link>
   </Layout>
 )
 
 export default IndexPage
+
